052acffbc59e7fc60657033b5b1bafe0b30001d0
Andrea Scagliarini <andrea.scagliarini1@gmail.com>
2024-03-22 15:30:06 +0100

    first commit

---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,9 @@
+<x-layout>
+    <div class="container">
+        <div class="row">
+            <div class="col-12 text-center">
+                <h1 class="display-1">Benvenuti su Spotibook</h1>
+            </div>
+        </div>
+    </div>
+</x-layout>
